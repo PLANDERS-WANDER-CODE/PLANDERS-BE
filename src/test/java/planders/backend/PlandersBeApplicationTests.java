9c51ea2d95ef9293dc45b9b262b2f42a73509098
Seungwon-Choi <bestwondev@gmail.com>
2025-01-15 16:47:59 +0900

    init: initialize spring boot

---
+++ src/test/java/planders/backend/PlandersBeApplicationTests.java
@@ -0,0 +1,13 @@
+package planders.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlandersBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

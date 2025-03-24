76c2257aeb7178e72d10d51930d8cceb3814eff9
Seungwon-Choi <bestwondev@gmail.com>
2025-03-24 16:52:53 +0900

    init: 프로젝트 초기 설정 #1

---
+++ src/main/java/com/planders/backend/PlandersBeApplication.java
@@ -0,0 +1,13 @@
+package com.planders.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlandersBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlandersBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planders/backend/PlandersBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planders.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlandersBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

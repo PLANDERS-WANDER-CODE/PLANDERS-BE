a3efe56de2562eeb91785169be31922217a64627
Seungwon-Choi <bestwondev@gmail.com>
2025-03-26 14:50:33 +0900

    test: Email 테스트 코드 추가

--- src/test/java/com/planders/backend/user/domain/EmailTest.java
+++ src/test/java/com/planders/backend/user/domain/EmailTest.java
@@ -1,6 +1,17 @@
 package com.planders.backend.user.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class EmailTest {
 
     // TODO Email 테스트
+
+    @DisplayName("이메일 값 null이면 NullPointerException 발생")
+    @Test
+    void createEmail_WithNullValue_ShouldThrowNullPointerException() {
+        assertThrows(NullPointerException.class, () -> Email.createEmail(null));
+    }
 }

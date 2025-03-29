feacfa869e945c86b20b22bf983f213a068b5fe9
hexter31376 <zwonyoung33@gmail.com>
2025-03-29 22:55:15 +0900

    test: Email 테스트 코드 추가 #2

--- src/test/java/com/planders/backend/user/domain/EmailTest.java
+++ src/test/java/com/planders/backend/user/domain/EmailTest.java
@@ -14,4 +14,20 @@ class EmailTest {
     void createEmail_WithNullValue_ShouldThrowNullPointerException() {
         assertThrows(NullPointerException.class, () -> Email.createEmail(null));
     }
+
+    @DisplayName("올바른 이메일 형식이면 예외가 발생하지 않는다")
+    @Test
+    void createEmail_WithValidEmail_ShouldNotThrow() {
+        assertDoesNotThrow(() -> Email.createEmail("zwonyoung@gwnu.ac.kr")); // . 두개 도메인 테스트
+        assertDoesNotThrow(() -> Email.createEmail("zwonyoung@gmail.com")); // . 한개 도메인 테스트
+        assertEquals("zwonyoung@gmail.com", Email.createEmail("zwonyoung@gmail.com").getEmailText());
+    }
+
+    @DisplayName("값이 정상적으로 들어갔는지 확인")
+    @Test
+    void GetEmailText_stateCheck_ShouldReturnEmailText() {
+        assertEquals("zwonyoung@gwnu.ac.kr", Email.createEmail("zwonyoung@gwnu.ac.kr").getEmailText());
+        assertEquals("zwonyoung@gmail.com", Email.createEmail("zwonyoung@gmail.com").getEmailText());
+        // 태스트 코드 추가 완료
+    }
 }
